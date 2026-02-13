7d1e8c0d80139d7d0d1a1bf42186d167672603a5
Peter Mooney <peter.moooney@nuim.ie>
2026-02-13 12:39:05 +0000

    creating a password copy

---
+++ PasswordsCopy.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+public class PasswordsCopy {
+    
+    public static void main(String args[])
+    {
+        System.out.println("Generating passwords"); 
+        Random randGen = new Random(); // our random class 
+        // Step 1
+        int n = 12; // password length
+        // make passwords more difficult to crack 
+
+        String password = ""; // our password String 
+
+        for (int i = 0; i < n; i++) // iterate n times // Step 2
+        {
+            //Each time we iterate use the Random class to 
+            // generate a random integer between 48 and 122 
+
+            int randNum = 48 + randGen.nextInt(75);
+            // cast the integer to a character - ASCII table 
+            char randChar = (char)(randNum);
+            // add character to password String 
+            password = password + randChar; 
+
+        } // end for 
+        // Step 3 - print password
+        System.out.println("Your password is " + password);  
+    }
+}
